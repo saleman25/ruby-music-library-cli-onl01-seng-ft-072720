44021eccd7a7582d682c0aa53cb2c94c2c0aa9db
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-15 02:13:24 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -23,10 +23,22 @@ while input != "exit"
  
  input = gets.strip
  
- case input
- 
- 
+  case input
+  when "list songs"
+    list_songs
+  when "list artists"
+    list_artists
+  when "list genres"
+    list_genres
+  when "list artist"
+    list_songs_by_artist
+  when "list genre"  
+    list_songs_by_genre
+  when "play song"
+    play_song
+end
 end 
+end
  
 def list_songs
  Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
