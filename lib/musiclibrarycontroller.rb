6b07e077bacb7c0aff6dc8cbee7c17926a140103
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-15 02:07:49 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -75,6 +75,7 @@ def play_song
   
   if (1..Song.all.length).include?(input)
   song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+  binding.pry
 end  
   puts "Playing #{song.name} by #{song.artist.name}" if song
 end 
