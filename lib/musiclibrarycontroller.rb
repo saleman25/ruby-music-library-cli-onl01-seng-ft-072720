428fde1a5d6b16f7511fb858090dbc647b8c9bba
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-15 02:09:17 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -8,9 +8,9 @@ end
 
 def call 
   
-#input = ""
+input = ""
   
-#while input != "exit"  
+while input != "exit"  
  puts "Welcome to your music library!"
  puts "To list all of your songs, enter 'list songs'."
  puts "To list all of the artists in your library, enter 'list artists'."
@@ -23,7 +23,7 @@ def call
  
  input = gets.strip
  
- #case input
+ case input
  
  
 end 
