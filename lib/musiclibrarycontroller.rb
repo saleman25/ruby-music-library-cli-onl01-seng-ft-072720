50e35c34c7a048d064b4a9abdb682d9d7852e134
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-15 01:36:14 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -35,9 +35,9 @@ end
 end  
 
 def list_artists
-  Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |name, i|
-  #binding.pry
-  puts "#{i}. #{@name} "
+  Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+  binding.pry
+  puts "#{i}. #{s.artist.name} "
  
 end 
 end 
