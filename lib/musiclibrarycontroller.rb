61dc8e81ae8c2f9ca0d178428157a5a1c177035d
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-15 00:54:31 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -5,6 +5,11 @@ def initialize(path = './db/mp3s')
 end  
 
 def call 
+  
+input = ""
+  
+while   
+  
  puts "Welcome to your music library!"
  puts "To list all of your songs, enter 'list songs'."
  puts "To list all of the artists in your library, enter 'list artists'."
